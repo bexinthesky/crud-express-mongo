0b4d59d9c3dda7397e7667f5764f6c029af0ae7c
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2018-04-06 15:22:43 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+console.log('May Node be with you');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.sendFile('/Users/rebeccahorwitz/Desktop/projects/crud-express-mongo' + '/index.html');
+    // console.log(_dirname);
+});
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body);
+});
+
+const MongoClient = require('mongodb').MongoClient;
+
+var db;
+
+MongoClient.connect('mongodb://R2-D2:whistlewhistlebeep@ds014118.mlab.com:14118/zellwk_tut', (err, client) => {
+    if (err) return console.log(err);
+    db = client.db('zellwk_tut');
+    app.listen(3000, () => {
+        console.log('listening to 3000');
+    });
+});
